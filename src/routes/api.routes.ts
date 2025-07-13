455ff3ae41e893d2f9971a9a7cd1857666279580
Alfredo <baptistaalfredo81@gmail.com>
2025-07-13 12:17:08 +0100

    fix: adicionado a routa para tarefas

--- src/routes/api.routes.ts
+++ src/routes/api.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import { errorHandler } from '../middlewares/errorHandler.middleware';
 import authRoutes from '@routes/auth.routes';
+import taskRoutes from '@routes/task.routes';
 
 const router = Router();
+
 router.use('/auth', authRoutes);
+router.use('/tasks', taskRoutes);
 router.use(errorHandler);
 export default router;
